bd9034f23ff1e9eb6b3e1a5d85e4db857c1a0406
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-09-12 21:59:46 +0300

    API config, create getLocation, getCurrentWeather and get5DayForecast functions, create types

---
+++ src/api/api.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { IParams, TLang, IGeocodingParams, TGeocodingResponse, IForecastParams, IForecastResponse, ICurrentWeatherResponse } from "./api-types";
+
+const API_KEY = '08018d1d6e27c1c72189ac0b68d9815a'
+const APP_LANG: TLang = 'ru'
+
+const instance = axios.create({
+	baseURL: 'https://api.openweathermap.org/data/2.5/',
+})
+
+export const weatherAPI = {
+	async getLocation(q: string = 'Stavropol') {
+		const params: IGeocodingParams = {
+			q,
+			appid: API_KEY
+		}
+		const res = await axios.get<TGeocodingResponse>('http://api.openweathermap.org/geo/1.0/direct', { params })
+		console.log(res)
+		return res
+	},
+	async getCurrentWeather(lat: number, lon: number) {
+		const params: IParams = {
+			lat,
+			lon,
+			appid: API_KEY,
+			lang: APP_LANG,
+			units: "metric"
+		}
+		const res = await instance.get<ICurrentWeatherResponse>('weather', { params })
+		console.log(res)
+		return res
+	},
+	async get5DayForecast(lat: number, lon: number, cnt?: number) {
+		const params: IForecastParams = {
+			lat,
+			lon,
+			appid: API_KEY,
+			units: 'metric',
+			lang: 'ru',
+			cnt
+		}
+		const res = await instance.get<IForecastResponse>('forecast', { params })
+		console.log(res)
+		return res
+	}
+}
