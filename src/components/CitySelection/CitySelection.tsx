5aff03de1168228b0cfbdbf151cd48a79787f567
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-09-13 15:38:51 +0300

    prettier test

---
+++ src/components/CitySelection/CitySelection.tsx
@@ -0,0 +1,12 @@
+export interface ICitySelectionProps {
+}
+
+const CitySelection: React.FC<ICitySelectionProps> = (props) => {
+	return (
+		<div>
+
+		</div>
+	)
+}
+
+export default CitySelection
