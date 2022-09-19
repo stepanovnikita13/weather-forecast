59794e5cacc9e61d03a3ad741168f5b4cfe64c10
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-09-19 20:15:01 +0300

    Change interface to type

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,5 +1,5 @@
-import { ITheme } from './theme'
+import { TTheme } from './theme'
 
 declare module 'styled-components' {
-   export interface DefaultTheme extends ITheme {}
+	export interface DefaultTheme extends TTheme {}
 }
