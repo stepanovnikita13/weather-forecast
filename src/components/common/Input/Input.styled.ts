c3d0ade14436d0f7ca736ff6daf06784818ae53f
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-09-19 21:30:12 +0300

    Config babel for styled components

--- src/components/common/Input/Input.styled.ts
+++ src/components/common/Input/Input.styled.ts
@@ -1,55 +1,55 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from 'styled-components/macro'
 
 export type TWrapper = {
-   fullwidth?: boolean
-   error?: boolean
+	fullwidth?: boolean
+	error?: boolean
 }
 
 export const Wrapper = styled.div<TWrapper>`
-   width: ${(props) => (props.fullwidth ? '100%' : '180px')};
+	width: ${(props) => (props.fullwidth ? '100%' : '180px')};
 `
 
 export type TBox = {
-   disabled?: boolean
+	disabled?: boolean
 }
 export const Box = styled.div<TBox>(
-   ({ disabled, theme }) => css`
-      display: flex;
-      align-items: center;
-      height: 38px;
-      padding: 0 10px;
-      border-radius: 8px;
-      background-color: ${disabled
-         ? theme.colors.backgroundDisabled
-         : theme.colors.backgroundContainer};
-      color: ${disabled ? theme.colors.fontDisabled : theme.colors.font};
-      border: 1px solid transparent;
-      -webkit-box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.2),
-         0px 1px 2px 0px rgba(0, 0, 0, 0.15);
-      -moz-box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.2),
-         0px 1px 2px 0px rgba(0, 0, 0, 0.15);
-      box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.1), 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+	({ disabled, theme }) => css`
+		display: flex;
+		align-items: center;
+		height: 38px;
+		padding: 0 10px;
+		border-radius: 8px;
+		background-color: ${disabled
+			? theme.colors.backgroundDisabled
+			: theme.colors.backgroundContainer};
+		color: ${disabled ? theme.colors.fontDisabled : theme.colors.font};
+		border: 1px solid transparent;
+		-webkit-box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.2),
+			0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+		-moz-box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.2),
+			0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+		box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.1), 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
 
-      &:hover {
-         border: 1px solid rgba(0, 0, 0, 0.15);
-      }
-   `
+		&:hover {
+			border: 1px solid rgba(0, 0, 0, 0.15);
+		}
+	`
 )
 
 export const Input = styled.input`
-   width: 100%;
-   border: none;
-   background-color: transparent;
-   color: inherit;
-   -webkit-tap-highlight-color: transparent;
+	width: 100%;
+	border: none;
+	background-color: transparent;
+	color: inherit;
+	-webkit-tap-highlight-color: transparent;
 
-   &:hover,
-   &:focus,
-   &:active {
-      outline: 0px;
-   }
+	&:hover,
+	&:focus,
+	&:active {
+		outline: 0px;
+	}
 `
 
 export const Label = styled.label`
-   //position: 'absolute';
+	//position: 'absolute';
 `
