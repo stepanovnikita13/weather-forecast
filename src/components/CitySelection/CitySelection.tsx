6c091c9f208d0ad3168b713f952e57968a96f934
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-09-28 23:18:47 +0300

    Add CitySelection component

--- src/components/CitySelection/CitySelection.tsx
+++ src/components/CitySelection/CitySelection.tsx
@@ -1,7 +1,15 @@
+import Input from '../common/Input/Input'
+
 export interface ICitySelectionProps {}
 
 const CitySelection: React.FC<ICitySelectionProps> = (props) => {
-   return <div></div>
+	return (
+		<div>
+			<h3>Type your location</h3>
+			<Input label='search' name='search' placeholder='Moscow' hideLabel fullwidth />
+			Location list here
+		</div>
+	)
 }
 
 export default CitySelection
