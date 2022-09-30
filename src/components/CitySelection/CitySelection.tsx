4cffab0612fccade482eae03b0993d9c057530e5
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-09-30 19:09:00 +0300

    Connent react-hook-form with submition logic

--- src/components/CitySelection/CitySelection.tsx
+++ src/components/CitySelection/CitySelection.tsx
@@ -1,14 +1,43 @@
+import Container from '../common/Container/Container'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 import Input from '../common/Input/Input'
+import React, { useEffect } from 'react'
 
 export interface ICitySelectionProps {}
+interface IFormInputs {
+	location: string
+}
 
 const CitySelection: React.FC<ICitySelectionProps> = (props) => {
+	const { handleSubmit, watch, control } = useForm<IFormInputs>()
+	const targetValue = watch('location')
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (targetValue) {
+				handleSubmit(onSubmit)()
+			}
+		}, 1000)
+		return () => clearTimeout(timer)
+	}, [targetValue, handleSubmit])
+
+	const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data)
+
 	return (
-		<div>
+		<Container pb={3}>
 			<h3>Type your location</h3>
-			<Input label='search' name='search' placeholder='Moscow' hideLabel fullwidth />
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Controller
+					name='location'
+					control={control}
+					defaultValue=''
+					render={({ field }) => (
+						<Input label='location' placeholder='Moscow' hideLabel fullwidth {...field} />
+					)}
+				/>
+			</form>
 			Location list here
-		</div>
+		</Container>
 	)
 }
 
