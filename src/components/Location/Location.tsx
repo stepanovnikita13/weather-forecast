60458ad87630b13840280c3a1517d8ceb6b17d1b
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-10-05 14:33:27 +0300

    Create location component

---
+++ src/components/Location/Location.tsx
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components/macro'
+import CitySelection from '../CitySelection/CitySelection'
+import { Container } from '../common/Containers/Containers'
+
+export interface ILocationProps {}
+
+const LocationContainer = styled.div(
+	({ theme }) => css`
+		height: 100%;
+		width: 100%;
+		background-color: ${theme.colors.backgrounds.sidebar};
+		overflow: hidden;
+	`
+)
+
+const Location: React.FC<ILocationProps> = (props) => {
+	return (
+		<LocationContainer>
+			<Container pb={2}>
+				<CitySelection />
+			</Container>
+			<h1>Location list</h1>
+		</LocationContainer>
+	)
+}
+
+export default Location
